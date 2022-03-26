2e72b334601a80b936e8217595b6b972e551f957
Gauarv-Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-03-26 11:33:30 +0530

    Intro to Arrays

---
+++ Array/arrayIntro.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class arrayIntro {
+
+	public static void main(String[] args) {
+//		int[] marks = new int[5];
+//		
+//		int[] maks;
+//		marks = new int[5];
+		
+//		int marks[]= new int[5];
+		
+		Scanner sc = new Scanner(System.in);
+//		int rollNo[] , marks[] ;
+		
+		int[] age = {2, 3, 21, 42, 12};
+		
+		
+//		double[] marks = {1.0, 22.5, 33.0};
+//		
+//		marks[1] = 4.56;
+//		
+//		System.out.println(marks[1]);
+		
+		for (int i = 0; i<age.length; i++) {
+			System.out.println(age[i]);
+		}
+	}
+
+}
