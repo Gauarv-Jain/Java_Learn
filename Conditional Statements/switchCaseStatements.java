3b2ed26926e7dba8ebf41361a0b9026aba1b70f1
Gauarv-Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-03-26 11:49:45 +0530

    switch case statements

---
+++ Conditional Statements/switchCaseStatements.java
@@ -0,0 +1,25 @@
+package ConditionalStatement;
+
+public class switchCaseStatements {
+
+	public static void main(String[] args) {
+		int d = 1;
+		
+		switch (d) {
+		case 1:
+			System.out.println("i am on leav");
+			break;
+		case 2 :
+			System.out.println("i am in office");
+			break;
+		case 3 :
+			System.out.println("office");
+			break;
+			default:
+				System.out.println("i dont know what day it is");
+	
+		}
+
+	}
+
+}
