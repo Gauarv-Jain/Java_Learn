a19e6e1b6c9cb00afda3b5334a69fe9bfb24f1f4
Gauarv-Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-03-26 11:48:52 +0530

    if else

---
+++ Conditional Statements/ifElse.java
@@ -0,0 +1,26 @@
+package ConditionalStatement;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+//		int age = 22;
+//		
+//		if( age >= 18) {
+//			System.out.println("you can vote now");
+//		
+//		}else {
+//				System.out.println("you canot vote");
+//			
+//		}
+		int a = 15;
+		int b = 55;
+		
+		int n = 0;
+		
+		n = (a>b)? a : b;
+		
+		System.out.println("max of both the numbers is " + n);
+		
+	}
+
+}
