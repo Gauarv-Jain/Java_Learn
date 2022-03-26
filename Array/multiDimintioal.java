31b9adbb668ec832b590543315fa89cc506fabe9
Gauarv-Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-03-26 11:45:04 +0530

    2-D arrays

---
+++ Array/multiDimintioal.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class multiDimintioal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+//		int a[][]= new int[5][3]; 
+//		
+//		System.out.println(a[2][5]);
+		
+		int b[][] = {
+				{3,4,2,6,4},
+				{3,7,1},
+				{1,6,3,7,8,3}
+					};
+		System.out.println(b[1][2]);
+
+	}
+
+}
