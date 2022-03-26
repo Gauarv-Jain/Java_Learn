4d6ef9549ec8140000532723ff038e7466466920
Gauarv-Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-03-26 11:46:53 +0530

    Different data types

---
+++ datatype/DataTypes.java
@@ -0,0 +1,15 @@
+package datatype;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int age=10;
+		float rateOfIntrust=2.3f;
+		double rate=1.54722;
+				
+				char lol ='$';
+				System.out.println(lol);
+
+	}
+
+}
