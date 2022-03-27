6a85acf76a4d0a954dc3df5fca7566662c16f566
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-03-27 22:04:43 +0530

    Add files via upload

---
+++ abstractionClass/Audi.java
@@ -0,0 +1,13 @@
+package abstractionClass;
+
+public class Audi extends Car{
+	
+	public void accelerate() {
+		System.out.println("Audi is accelerating");
+	}
+	
+	public void breaking() {
+		System.out.println("Audi is breaking");
+	}
+
+}
