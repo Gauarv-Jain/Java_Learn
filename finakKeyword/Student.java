f1c37db06a3a7018e299faceff5bb60fd8da0ea6
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-04-02 23:03:16 +0530

    Add files via upload

---
+++ finakKeyword/Student.java
@@ -0,0 +1,14 @@
+package finakKeyword;
+
+public class Student {
+	
+	int rollNo;
+	String name;
+	
+	public final void getDescription() {
+		System.out.println("the student name is  " + name);
+	}
+	
+}
+	
+	
