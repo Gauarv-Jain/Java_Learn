93004afdeb3280c0c6bd124b01d09e566e1f3329
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-04-02 23:02:47 +0530

    Add files via upload

---
+++ finakKeyword/MainClass.java
@@ -0,0 +1,17 @@
+package finakKeyword;
+
+public class MainClass extends Student {
+	
+//	public void getDescription() {
+//		System.out.println("inside mian class");
+//	}
+	
+	public static void main(String[] args) {
+	
+		MainClass obj = new MainClass();
+		
+		obj.getDescription();
+
+	}
+
+}
