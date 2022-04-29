45b520130fcd3fc9ffe88fcac4f1e33726f16742
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-04-29 11:46:42 +0530

    Add files via upload

---
+++ oops/packages/MainClass.java
@@ -0,0 +1,19 @@
+package oops.packages;
+
+import java.util.Scanner;
+
+import oops.packages.models.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Student obj = new Student("tom");
+		
+		Teacher teacher = new Teacher();
+		
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
