e10df282c4f67ef90b4ac013ac5b086df9e942ca
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-05-20 23:02:14 +0530

    Add files via upload

---
+++ queue/MainClass.java
@@ -0,0 +1,47 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.add(12);
+		q.add(23);
+		q.add(34);
+		
+		System.out.println(q);
+		
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		
+		System.out.println(q.element());
+		
+		System.out.println(q.poll());
+		
+		System.out.println(q.poll());
+		
+		
+		MyQueue<Integer> mq = new MyQueue<>();
+		
+		mq.enqueue(12);
+		mq.enqueue(2);
+		mq.enqueue(123);
+		mq.enqueue(45);
+		
+		System.out.println(mq.dequeue());
+		System.out.println(mq.dequeue());
+		System.out.println(mq.dequeue());
+		System.out.println(mq.dequeue());
+		System.out.println(mq.dequeue());
+		
+		mq.enqueue(451);
+		
+		System.out.println(mq.dequeue());
+
+	}
+
+}
