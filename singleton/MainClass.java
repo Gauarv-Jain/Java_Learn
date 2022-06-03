bb6fdfbfad5da7bbcb3296f15150e84d7f62e396
Gaurav Jain <92380558+Gauarv-Jain@users.noreply.github.com>
2022-06-03 22:45:05 +0530

    Add files via upload

---
+++ singleton/MainClass.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+//		AppConfig obj = new AppConfig();
+		
+		AppConfig obj = AppConfig.getInstace();
+
+	}
+
+}
